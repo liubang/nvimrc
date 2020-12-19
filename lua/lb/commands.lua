c2566ba8fda8abeca11673d495152f2911c3dc37
liubang <it.liubang@gmail.com>
2020-12-20 01:09:07 +0800

    update

--- lua/lb/commands.lua
+++ lua/lb/commands.lua
@@ -1,16 +1,18 @@
 --=====================================================================
 --
--- commands.lua - 
+-- commands.lua -
 --
 -- Created by liubang on 2020/12/12 18:32
 -- Last Modified: 2020/12/12 18:32
 --
 --=====================================================================
 
-vim.schedule(function()
-  vim.cmd [[command! -nargs=0 -bar PlugUpdate call dein#update()]]
-  vim.cmd [[command! -nargs=0 -bar PlugClean  call map(dein#check_clean(), "delete(v:val, 'rf')")]]
-  vim.cmd [[command! -nargs=0 -bar ReRuntimePath  call dein#recache_runtimepath()]]
-  vim.cmd [[command! -nargs=0 CopyRight :lua require('lb.utils.comment').copy_right('liubang')]]
-  vim.cmd [[command! -nargs=0 CopyRightUpdate :lua require('lb.utils.comment').copy_right_update()]]
-end)
+vim.schedule(
+  function()
+    vim.cmd [[command! -nargs=0 -bar PlugUpdate call dein#update()]]
+    vim.cmd [[command! -nargs=0 -bar PlugClean  call map(dein#check_clean(), "delete(v:val, 'rf')")]]
+    vim.cmd [[command! -nargs=0 -bar ReRuntimePath  call dein#recache_runtimepath()]]
+    vim.cmd [[command! -nargs=0 CopyRight :lua require('lb.utils.comment').copy_right('liubang')]]
+    vim.cmd [[command! -nargs=0 CopyRightUpdate :lua require('lb.utils.comment').copy_right_update()]]
+  end
+)
--- lua/lb/init.lua
+++ lua/lb/init.lua
@@ -1,6 +1,6 @@
 --=====================================================================
 --
--- init.lua - 
+-- init.lua -
 --
 -- Created by liubang on 2020/12/11
 -- Last Modified: 2020/12/11 00:23
@@ -11,12 +11,12 @@ local g, fn = vim.g, vim.fn
 
 return function(v)
   g.nvg_version = v
-  g.nvg_root = fn.stdpath('config')
-  g.cache_path  = g.nvg_root .. '/.cache'
-  g.module_path = g.nvg_root .. '/modules'
-  g.snip_path   = g.nvg_root .. '/snippets'
+  g.nvg_root = fn.stdpath("config")
+  g.cache_path = g.nvg_root .. "/.cache"
+  g.module_path = g.nvg_root .. "/modules"
+  g.snip_path = g.nvg_root .. "/snippets"
 
-  local p = assert(io.popen('find "' .. g.module_path ..'" -name "*.toml"'))
+  local p = assert(io.popen('find "' .. g.module_path .. '" -name "*.toml"'))
   local modules = {}
   for file in p:lines() do
     table.insert(modules, file)
@@ -24,10 +24,10 @@ return function(v)
 
   -- plugins
   -- require('lb.plugins')
-  require('lb.utils.pm').setup(modules)
+  require("lb.utils.pm").setup(modules)
 
-  require('lb.options')
-  require('lb.mappings')
-  require('lb.events')
-  require('lb.commands')
+  require("lb.options")
+  require("lb.mappings")
+  require("lb.events")
+  require("lb.commands")
 end
