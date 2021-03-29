a5c0b22c7180a766f1dbb619b3dc43030570ef61
liubang <it.liubang@gmail.com>
2021-03-29 14:08:43 +0800

    update

--- lua/lb/config/lsp/handlers.lua
+++ lua/lb/config/lsp/handlers.lua
@@ -9,7 +9,7 @@
 -- LuaFormatter off
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true, 
+    virtual_text = false, 
     underline = true,
     signs = true, 
     update_in_insert = false
