6930537e755e46fe073e2981322cb243a5b9577f
liubang <liubang01@baidu.com>
2021-07-14 16:31:17 +0800

    update

--- lua/lb/plugins/plugins.lua
+++ lua/lb/plugins/plugins.lua
@@ -107,7 +107,6 @@ use {
 }
 use {
   'liuchengxu/vista.vim',
-  cmd = {'Vista', 'Vista!', 'Vista!!'},
   config = function()
     vim.g.vista_echo_cursor = 0
     vim.g.vista_default_executive = 'nvim_lsp'
