051611b2c13e87b23925ac45b6268ea1a1783353
liubang <it.liubang@gmail.com>
2021-08-31 22:43:02 +0800

    update

---
+++ after/plugin/comment.lua
@@ -0,0 +1,10 @@
+-- =====================================================================
+--
+-- comment.lua - 
+--
+-- Created by liubang on 2021/08/31 22:41
+-- Last Modified: 2021/08/31 22:41
+--
+-- =====================================================================
+
+require('kommentary.config').configure_language('default', {prefer_single_line_comments = true})
