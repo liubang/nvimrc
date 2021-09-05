75f714b8e8b58160ce2e5c720d1fedf0f0f5f76c
liubang <it.liubang@gmail.com>
2021-09-05 13:58:06 +0800

    update

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,10 +1,15 @@
 -- =====================================================================
 --
--- git.lua - 
+-- git.lua -
 --
 -- Created by liubang on 2021/08/29 03:52
 -- Last Modified: 2021/08/29 03:52
 --
 -- =====================================================================
 
-require('gitsigns').setup({keymaps = {}})
+require('gitsigns').setup {
+  keymaps = {
+    noremap = true,
+    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+  },
+}
