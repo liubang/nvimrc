cbeeb3d491e509f01bc666769b3f38ceb07a256b
liubang <liubang01@baidu.com>
2022-01-11 10:50:08 +0800

    update

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -116,6 +116,14 @@ cmp.setup {
 }
 
 -- autopairs
+require('nvim-autopairs').setup {
+  check_ts = true,
+  ts_config = {
+    lua = { 'string', 'source' },
+    javascript = { 'string', 'template_string' },
+    java = false,
+  },
+  disable_filetype = { 'TelescopePrompt', 'vim' },
+}
 local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-require('nvim-autopairs').setup { disable_filetype = { 'TelescopePrompt', 'vim' } }
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done { map_char = { tex = '' } })
