1d307e4e341b4f0821f3277e0d3db4769ad6a135
liubang <it.liubang@gmail.com>
2022-01-18 23:51:31 +0800

    update

--- lua/lb/lsp/handlers.lua
+++ lua/lb/lsp/handlers.lua
@@ -6,18 +6,14 @@
 -- Last Modified: 2021/02/10 10:06
 --
 -- =====================================================================
-vim.lsp.handlers['textDocument/definition'] = function(_, result)
-  if not result or vim.tbl_isempty(result) then
-    print '[LSP] Could not find definition'
-    return
-  end
+local saga = require 'lspsaga'
 
-  if vim.tbl_islist(result) then
-    vim.lsp.util.jump_to_location(result[1])
-  else
-    vim.lsp.util.jump_to_location(result)
-  end
-end
+saga.init_lsp_saga {
+  error_sign = '',
+  warn_sign = '',
+  hint_sign = '',
+  infor_sign = '',
+}
 
 vim.diagnostic.config {
   underline = false,
@@ -27,30 +23,19 @@ vim.diagnostic.config {
   severity_sort = true,
 }
 
-vim.lsp.handlers['textDocument/hover'] = require('lspsaga.hover').handler
-
-local ns_rename = vim.api.nvim_create_namespace 'lb_rename'
-
-MyLspRename = function()
-  local bufnr = vim.api.nvim_get_current_buf()
-  vim.api.nvim_buf_clear_namespace(bufnr, ns_rename, 0, -1)
-  local current_word = vim.fn.expand '<cword>'
-  local plenary_window = require('plenary.window.float').percentage_range_window(0.5, 0.2)
-  vim.api.nvim_buf_set_option(plenary_window.bufnr, 'buftype', 'prompt')
-  vim.fn.prompt_setprompt(plenary_window.bufnr, string.format('Rename "%s" to > ', current_word))
-  vim.fn.prompt_setcallback(plenary_window.bufnr, function(text)
-    vim.api.nvim_win_close(plenary_window.win_id, true)
-    if text ~= '' then
-      vim.schedule(function()
-        vim.api.nvim_buf_delete(plenary_window.bufnr, { force = true })
-        vim.lsp.buf.rename(text)
-      end)
-    else
-      print 'Nothing to rename!'
-    end
-  end)
-  vim.cmd [[startinsert]]
+vim.lsp.handlers['textDocument/definition'] = function(_, result)
+  if not result or vim.tbl_isempty(result) then
+    print '[LSP] Could not find definition'
+    return
+  end
+  if vim.tbl_islist(result) then
+    vim.lsp.util.jump_to_location(result[1])
+  else
+    vim.lsp.util.jump_to_location(result)
+  end
 end
+
+vim.lsp.handlers['textDocument/hover'] = require('lspsaga.hover').handler
 
 -- golang organize imports
 GoOrgImports = function(timeoutms)
