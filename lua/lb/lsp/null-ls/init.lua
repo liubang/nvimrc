f588a72b863cc718e0c7604558a5f223fa5c6c61
liubang <liubang01@baidu.com>
2022-02-25 15:36:41 +0800

    update

--- lua/lb/lsp/null-ls/init.lua
+++ lua/lb/lsp/null-ls/init.lua
@@ -9,9 +9,11 @@
 local null_ls = require 'null-ls'
 
 require('null-ls').setup {
+  on_attach = require('lb.lsp.customs').default({}).on_attach,
   sources = {
     require 'lb.lsp.null-ls.formatting.buildifier',
     -- null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.asmfmt,
     null_ls.builtins.formatting.shfmt.with {
       extra_args = { '-i', '2', '-ci' },
     },
