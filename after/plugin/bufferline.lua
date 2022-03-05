40fa0a43b36e46d6ae4ffc6a543d4c20858eb4be
liubang <it.liubang@gmail.com>
2022-03-05 20:05:45 +0800

    update

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,6 +1,6 @@
 --=====================================================================
 --
--- bufferline.lua - 
+-- bufferline.lua -
 --
 -- Created by liubang on 2022/03/02 16:58
 -- Last Modified: 2022/03/02 16:58
@@ -8,11 +8,21 @@
 --=====================================================================
 require('bufferline').setup {
   options = {
-    numbers = 'ordinal',
+    numbers = 'both',
     close_icon = '\u{f18a}',
     modified_icon = '\u{fbba}',
     diagnostics = false,
+    left_trunc_marker = '',
+    right_trunc_marker = '',
+    indicator_icon = '▎',
+    buffer_close_icon = '',
+    tab_size = 20,
+    max_name_length = 20,
+    max_prefix_length = 15,
+    show_close_icon = true,
+    show_buffer_icons = true,
     show_buffer_close_icons = false,
     always_show_bufferline = true,
+    show_tab_indicators = true,
   },
 }
