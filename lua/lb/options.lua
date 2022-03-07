245b4123efad5f6411a0a28e38f474d96cd4567f
liubang <liubang01@baidu.com>
2022-03-07 10:14:23 +0800

    update

--- lua/lb/options.lua
+++ lua/lb/options.lua
@@ -121,7 +121,7 @@ opt.cursorline = true
 opt.cursorcolumn = false
 
 --- {{{neowide
-vim.cmd [[set guifont=Operator_Mono_Lig_Book:h12,Hack_Nerd_Font:h12]]
+vim.cmd [[set guifont=Operator\ Mono\ Lig:h16,Hack\ Nerd\ Font:h16]]
 vim.g.neovide_refresh_rate = 60
 vim.g.neovide_cursor_vfx_mode = 'railgun'
 vim.g.neovide_no_idle = true
