3a2460b767a9018f2b32db059d7f7e8d488d04da
liubang <it.liubang@gmail.com>
2022-03-29 23:15:00 +0800

    update

--- lua/lb/lsp/servers.lua
+++ lua/lb/lsp/servers.lua
@@ -87,6 +87,39 @@ local function get_jdtls_config()
   return util.path.join(vim.fn.stdpath 'data', '/lsp_servers/jdtls/config_linux')
 end
 
+local function progress_report(_, result, ctx)
+  local info = {
+    client_id = ctx.client_id,
+  }
+
+  local kind = 'report'
+  if result.complete then
+    kind = 'end'
+  elseif result.workDone == 0 then
+    kind = 'begin'
+  elseif result.workDone > 0 and result.workDone < result.totalWork then
+    kind = 'report'
+  else
+    kind = 'end'
+  end
+
+  local percentage = 0
+  if result.totalWork > 0 and result.workDone >= 0 then
+    percentage = result.workDone / result.totalWork * 100
+  end
+
+  local msg = {
+    token = result.id,
+    value = {
+      kind = kind,
+      percentage = percentage,
+      title = result.subTask,
+      message = result.subTask,
+    },
+  }
+  vim.lsp.handlers['$/progress'](nil, msg, info)
+end
+
 local function get_jdtls_options(server)
   local opts = {
     name = 'jdtls',
@@ -116,6 +149,9 @@ local function get_jdtls_options(server)
         vim.fn.getcwd(),
         ':p:h:t'
       ),
+    },
+    handlers = {
+      ['language/progressReport'] = progress_report,
     },
     settings = {
       java = {
