2118751552a050a83d17222e7eb060ee724af47f
liubang <it.liubang@gmail.com>
2022-07-10 18:22:21 +0800

    update

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -11,9 +11,22 @@ local wk = require 'which-key'
 local mp = require 'lb.mappings'
 
 wk.setup {
+  plugins = {
+    marks = true, -- shows a list of your marks on ' and `
+    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    presets = {
+      operators = false, -- adds help for operators like d, y, ...
+      motions = false, -- adds help for motions
+      text_objects = false, -- help for text objects triggered after entering an operator
+      windows = true,
+      nav = true, -- misc bindings to work with windows
+      z = true, -- bindings for folds, spelling and others prefixed with z
+      g = true, -- bindings for prefixed with g
+    },
+  },
   icons = {
     breadcrumb = '»', -- symbol used in the command line area that shows your active key combo
-    separator = '  ', -- symbol used between a key and it's label
+    separator = '➜', -- symbol used between a key and it's label
     group = '+', -- symbol prepended to a group
   },
 
