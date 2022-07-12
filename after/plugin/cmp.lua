da96ffb211cdb83c662241ec43fd505b89d36192
liubang <it.liubang@gmail.com>
2022-07-12 10:32:42 +0800

    update

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -126,4 +126,4 @@ require('nvim-autopairs').setup {
 }
 
 local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done { map_char = { tex = '' } })
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
