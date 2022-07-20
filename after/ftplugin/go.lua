14b1ac94af69d46bb59f523ab29a8b31cfc63eac
liubang <it.liubang@gmail.com>
2022-07-20 15:10:54 +0800

    update

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -6,4 +6,4 @@
 -- Last Modified: 2022/03/28 14:06
 --
 --=====================================================================
-vim.cmd 'setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4'
+vim.cmd 'setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2'
