90fd6fefd44e313e0f34c51ab08fb9994923f3d2
liubang <it.liubang@gmail.com>
2022-07-20 15:38:09 +0800

    update

--- lua/lb/lsp/servers.lua
+++ lua/lb/lsp/servers.lua
@@ -68,26 +68,44 @@ lspconfig.gopls.setup(c.default {
   -- share the gopls instance if there is one already
   cmd = { 'gopls', '-remote.debug=:0' },
   filetypes = { 'go', 'gomod', 'gosum', 'gotmpl', 'gohtmltmpl', 'gotexttmpl' },
+  message_level = vim.lsp.protocol.MessageType.Error,
   flags = { allow_incremental_sync = true, debounce_text_changes = 500 },
   settings = {
     gopls = {
-      experimentalPostfixCompletions = true,
+      -- more settings: https://github.com/golang/tools/blob/master/gopls/doc/settings.md
       analyses = {
         unusedparams = true,
         unreachable = true,
+        nilness = true,
+        useany = true,
+        unusedwrite = true,
+        ST1003 = true,
+        undeclaredname = true,
+        fillreturns = true,
+        nonewvars = true,
+        fieldalignment = false,
         shadow = true,
       },
       codelenses = {
-        generate = true,
-        gc_details = true,
+        generate = true, -- show the `go generate` lens.
+        gc_details = true, -- Show a code lens toggling the display of gc's choices.
         test = true,
         tidy = true,
+        vendor = true,
+        regenerate_cgo = true,
+        upgrade_dependency = true,
       },
+      experimentalUseInvalidMetadata = true,
+      hoverKind = 'Structured',
       usePlaceholders = true,
       completeUnimported = true,
       staticcheck = true,
-      gofumpt = true,
-      ['local'] = '',
+      matcher = 'Fuzzy',
+      diagnosticsDelay = '500ms',
+      experimentalWatchedFileDelay = '200ms',
+      symbolMatcher = 'fuzzy',
+      gofumpt = true, -- true|false, -- turn on for new repos, gofmpt is good but also create code turmoils
+      buildFlags = { '-tags', 'integration' },
     },
   },
 })
