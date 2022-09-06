f05c3f0377f4f4a978d6778fb185f6fb6ad4662c
liubang <it.liubang@gmail.com>
2022-09-06 23:36:58 +0800

    update

--- lua/lb/plugins/bufferline.lua
+++ lua/lb/plugins/bufferline.lua
@@ -15,8 +15,8 @@ require('bufferline').setup {
     right_mouse_command = 'Bdelete',
     middle_mouse_command = nil,
     indicator = {
-      icon = '',
-      style = 'none',
+      icon = '▎',
+      style = 'icon',
     },
     buffer_close_icon = '',
     modified_icon = '●',
@@ -40,97 +40,97 @@ require('bufferline').setup {
     always_show_bufferline = true,
     sort_by = 'insert_at_end',
   },
-  highlights = {
-    fill = {
-      bg = '#3c3836',
-      fg = '#ddc7a1',
-    },
-    background = {
-      bg = '#5b534d',
-      fg = '#ddc7a1',
-    },
-    tab_selected = {
-      fg = '#32302f',
-      bg = '#a89984',
-    },
-    buffer_visible = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-    },
-    buffer_selected = {
-      fg = '#32302f',
-      bg = '#a89984',
-      bold = true,
-      italic = false,
-    },
-    numbers = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-    },
-    numbers_visible = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-    },
-    numbers_selected = {
-      fg = '#32302f',
-      bg = '#a89984',
-      bold = true,
-      italic = false,
-    },
-    duplicate_selected = {
-      fg = '#32302f',
-      bg = '#a89984',
-      italic = false,
-    },
-    duplicate_visible = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-      italic = false,
-    },
-    duplicate = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-      italic = false,
-    },
-    close_button = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-    },
-    close_button_visible = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-    },
-    close_button_selected = {
-      fg = '#32302f',
-      bg = '#a89984',
-    },
-    separator_visible = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-    },
-    separator = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-    },
-    separator_selected = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-    },
-    indicator_selected = {
-      fg = '#32302f',
-      bg = '#a89984',
-    },
-    modified = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-    },
-    modified_visible = {
-      fg = '#ddc7a1',
-      bg = '#5b534d',
-    },
-    modified_selected = {
-      fg = '#32302f',
-      bg = '#a89984',
-    },
-  },
+  -- highlights = {
+  --   fill = {
+  --     bg = '#3c3836',
+  --     fg = '#ddc7a1',
+  --   },
+  --   background = {
+  --     bg = '#5b534d',
+  --     fg = '#ddc7a1',
+  --   },
+  --   tab_selected = {
+  --     fg = '#32302f',
+  --     bg = '#a89984',
+  --   },
+  --   buffer_visible = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --   },
+  --   buffer_selected = {
+  --     fg = '#32302f',
+  --     bg = '#a89984',
+  --     bold = true,
+  --     italic = false,
+  --   },
+  --   numbers = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --   },
+  --   numbers_visible = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --   },
+  --   numbers_selected = {
+  --     fg = '#32302f',
+  --     bg = '#a89984',
+  --     bold = true,
+  --     italic = false,
+  --   },
+  --   duplicate_selected = {
+  --     fg = '#32302f',
+  --     bg = '#a89984',
+  --     italic = false,
+  --   },
+  --   duplicate_visible = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --     italic = false,
+  --   },
+  --   duplicate = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --     italic = false,
+  --   },
+  --   close_button = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --   },
+  --   close_button_visible = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --   },
+  --   close_button_selected = {
+  --     fg = '#32302f',
+  --     bg = '#a89984',
+  --   },
+  --   separator_visible = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --   },
+  --   separator = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --   },
+  --   separator_selected = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --   },
+  --   indicator_selected = {
+  --     fg = '#32302f',
+  --     bg = '#a89984',
+  --   },
+  --   modified = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --   },
+  --   modified_visible = {
+  --     fg = '#ddc7a1',
+  --     bg = '#5b534d',
+  --   },
+  --   modified_selected = {
+  --     fg = '#32302f',
+  --     bg = '#a89984',
+  --   },
+  -- },
 }
