f7eb15956cffdb4aa72963e26d95498c2fe3d706
liubang <it.liubang@gmail.com>
2022-11-12 20:53:12 +0800

    update

--- lua/lb/snips/all.lua
+++ lua/lb/snips/all.lua
@@ -29,6 +29,7 @@ end
 local snippets = {
   ls.s('time', partial(vim.fn.strftime, '%H:%M:%S')),
   ls.s('date', partial(vim.fn.strftime, '%Y-%m-%d')),
+  ls.s('datetime', partial(vim.fn.strftime, '%Y-%m-%d %H:%M:%S')),
   ls.s('pwd', { partial(util.shell, 'pwd') }),
   ls.s({ trig = 'uuid', wordTrig = true }, { ls.f(util.uuid), ls.i(0) }),
   ls.s('shrug', { ls.t '¯\\_(ツ)_/¯' }),
