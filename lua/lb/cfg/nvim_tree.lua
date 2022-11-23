d1642b481def2bfb3cc2692beb5b01b906f6cd57
liubang <it.liubang@gmail.com>
2022-11-24 00:02:39 +0800

    update nvim-tree config

--- lua/lb/cfg/nvim_tree.lua
+++ lua/lb/cfg/nvim_tree.lua
@@ -11,17 +11,18 @@ local nvim_tree = require 'nvim-tree'
 
 nvim_tree.setup {
   disable_netrw = true,
-  hijack_netrw = true,
   open_on_setup = false,
   open_on_tab = false,
-  hijack_cursor = false,
   update_cwd = false,
   auto_reload_on_write = true,
   respect_buf_cwd = false,
+  hijack_cursor = true,
+  hijack_netrw = true,
+  hijack_unnamed_buffer_when_opening = false,
+  ignore_ft_on_setup = { 'alpha' },
   update_focused_file = {
     enable = true,
     update_cwd = false,
-    ignore_list = {},
   },
   diagnostics = {
     enable = false,
@@ -31,11 +32,31 @@ nvim_tree.setup {
     ignore = true,
     timeout = 500,
   },
+  filesystem_watchers = {
+    enable = true,
+  },
+  actions = {
+    open_file = {
+      resize_window = true,
+    },
+  },
   renderer = {
     highlight_git = true,
     group_empty = true,
     icons = {
       glyphs = {
+        default = '',
+        symlink = '',
+        folder = {
+          default = '',
+          empty = '',
+          empty_open = '',
+          open = '',
+          symlink = '',
+          symlink_open = '',
+          arrow_open = '',
+          arrow_closed = '',
+        },
         git = { --{{{
           deleted = '',
           ignored = '◌',
@@ -45,21 +66,14 @@ nvim_tree.setup {
           unstaged = '',
           untracked = '★',
         }, --}}}
-        folder = { --{{{
-          arrow_open = '▾',
-          arrow_closed = '▸',
-        }, --}}}
       },
     },
   },
   view = {
     width = 35,
     side = 'left',
-    number = false,
-    relativenumber = false,
+    adaptive_size = true,
     hide_root_folder = false,
-    preserve_window_proportions = false,
-    signcolumn = 'yes',
     mappings = {
       custom_only = true,
       list = {
@@ -88,13 +102,13 @@ end)
 
 vim.keymap.set('n', '<Leader>ft', nvim_tree.toggle, { silent = true, desc = 'toggle tree view' })
 
-vim.api.nvim_create_autocmd('BufEnter', { --{{{
-  nested = true,
-  callback = function()
-    if vim.fn.winnr '$' == 1 and vim.fn.bufname() == 'NvimTree_' .. vim.fn.tabpagenr() then
-      vim.api.nvim_command ':silent q'
-    end
-  end,
-}) --}}}
+-- vim.api.nvim_create_autocmd('BufEnter', { --{{{
+--   nested = true,
+--   callback = function()
+--     if vim.fn.winnr '$' == 1 and vim.fn.bufname() == 'NvimTree_' .. vim.fn.tabpagenr() then
+--       vim.api.nvim_command ':silent q'
+--     end
+--   end,
+-- }) --}}}
 
 -- vim: fdm=marker fdl=0
