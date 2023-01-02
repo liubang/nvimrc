280dc8f75bd8d32a9510a05c724a744fc14f20d3
liubang <it.liubang@gmail.com>
2023-01-02 23:25:31 +0800

    feat(autocmd): automatically jump to the last place you’ve visited in a file before exiting

--- lua/lb/autocmd.lua
+++ lua/lb/autocmd.lua
@@ -61,23 +61,13 @@ vim.api.nvim_create_autocmd("BufNewFile", {
 }) -- }}}
 
 -- go to last loc when opening a buffer {{{
-vim.api.nvim_create_autocmd("BufReadPre", {
-  pattern = "*",
-  group = special_settings_group,
+vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "<buffer>",
-      once = true,
-      callback = function()
-        local filetype = vim.api.nvim_buf_get_option(0, "filetype")
-        if filetype ~= "commit" and filetype ~= "rebase" then
-          local mark = vim.fn.line "'\""
-          if mark > 1 and mark <= vim.fn.line "$" then
-            vim.cmd 'normal! g`"'
-          end
-        end
-      end,
-    })
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
   end,
 }) -- }}}
 
