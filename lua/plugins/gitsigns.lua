7c85aa882c021baaaf4ed749124455d8e2660812
liubang <it.liubang@gmail.com>
2023-01-03 19:07:00 +0800

    ref(gitsigns): make code looks more beautiful

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,6 +6,8 @@
 -- Last Modified: 2022/12/30 22:21
 --
 --=====================================================================
+
+-- stylua: ignore start
 return {
   "lewis6991/gitsigns.nvim",
   event = "BufReadPre",
@@ -14,23 +16,23 @@ return {
     local gitsigns = require "gitsigns"
     gitsigns.setup {
       signs = {
-        add = { text = "▌", show_count = true },
-        change = { text = "▌", show_count = true },
-        delete = { text = "▐", show_count = true },
-        topdelete = { text = "▛", show_count = true },
+        add          = { text = "▌", show_count = true },
+        change       = { text = "▌", show_count = true },
+        delete       = { text = "▐", show_count = true },
+        topdelete    = { text = "▛", show_count = true },
         changedelete = { text = "▚", show_count = true },
       },
       sign_priority = 10,
       count_chars = {
-        [1] = "",
-        [2] = "₂",
-        [3] = "₃",
-        [4] = "₄",
-        [5] = "₅",
-        [6] = "₆",
-        [7] = "₇",
-        [8] = "₈",
-        [9] = "₉",
+        [1]   = "",
+        [2]   = "₂",
+        [3]   = "₃",
+        [4]   = "₄",
+        [5]   = "₅",
+        [6]   = "₆",
+        [7]   = "₇",
+        [8]   = "₈",
+        [9]   = "₉",
         ["+"] = "₊",
       },
       preview_config = {
@@ -46,5 +48,7 @@ return {
     }
   end,
 }
+
+-- stylua: ignore end
 
 -- vim: fdm=marker fdl=0
