1fd19b54d937f8851929a452a9544122e130d6db
liubang <it.liubang@gmail.com>
2023-04-22 00:32:21 +0800

    fix(bufferline): deprecate show_buffer_default_icon

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -28,9 +28,7 @@ return {
       diagnostics = false,
       diagnostics_update_in_insert = false,
       color_icons = true,
-      show_buffer_icons = true,
       show_buffer_close_icons = false,
-      show_buffer_default_icon = true,
       show_close_icon = false,
       show_tab_indicators = false,
       persist_buffer_sort = true,
@@ -38,6 +36,10 @@ return {
       enforce_regular_tabs = true,
       always_show_bufferline = false,
       sort_by = "insert_at_end",
+      get_element_icon = function(element)
+        local icon, hl = require("nvim-web-devicons").get_icon_by_filetype(element.filetype, { default = false })
+        return icon, hl
+      end,
     }, -- }}}
   },
   keys = {
