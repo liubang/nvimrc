d5221a48617b4d81431413845ba7ccddd5745061
liubang <it.liubang@gmail.com>
2023-05-08 00:34:45 +0800

    perf(mini.lua): remove mini.ai keys config

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -85,13 +85,10 @@ return {
         end,
       },
     },
-    keys = {
-      { "a", mode = { "x", "o" } },
-      { "i", mode = { "x", "o" } },
-    },
     opts = function()
       local ai = require "mini.ai"
       return {
+        n_lines = 500,
         custom_textobjects = {
           o = ai.gen_spec.treesitter({
             a = { "@block.outer", "@conditional.outer", "@loop.outer" },
