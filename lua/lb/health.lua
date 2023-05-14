a63d3c6011c67f5d82cf5fa2430311536a0ede2a
liubang <it.liubang@gmail.com>
2023-05-14 13:45:55 +0800

    perf(health): fix deprecated functions

--- lua/lb/health.lua
+++ lua/lb/health.lua
@@ -12,14 +12,14 @@ local health = vim.health or require "health"
 
 local check_executable = function(bin, name, advice) -- {{{
   if vim.fn.executable(bin) == 0 then
-    health.report_error(string.format("Please install %s, %s", name, advice))
+    health.error(string.format("Please install %s, %s", name, advice))
   else
-    health.report_ok(string.format("%s is installed", name))
+    health.ok(string.format("%s is installed", name))
   end
 end -- }}}
 
 M.check = function() -- {{{
-  health.report_start "Checking nvim configuration requirements"
+  health.start "Checking nvim configuration requirements"
   check_executable("yarn", "yarn", "Refer to https://classic.yarnpkg.com/en/docs/install")
   check_executable("rg", "ripgrep", "Refer to https://github.com/BurntSushi/ripgrep#installation")
 end -- }}}
