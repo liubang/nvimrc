abf8d4f612e86f82756755566f964e22d2cf3f6f
liubang <it.liubang@gmail.com>
2023-05-31 14:49:45 +0800

    perf(theme): disable italic comment

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -42,6 +42,7 @@ return {
       vim.g.gruvbox_material_disable_italic_comment = 0
       vim.g.gruvbox_material_background = "soft"
       vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_disable_italic_comment = 1
       -- }}}
 
       vim.cmd.colorscheme "gruvbox-material"
