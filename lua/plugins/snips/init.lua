13d812683142c721cd123079b7c3a2167611a63c
liubang <it.liubang@gmail.com>
2023-10-25 18:09:17 +0800

    fix(LuaSnip): add friendly-snippets

--- lua/plugins/snips/init.lua
+++ lua/plugins/snips/init.lua
@@ -9,6 +9,7 @@
 
 return {
     'L3MON4D3/LuaSnip',
+    dependencies = { 'rafamadriz/friendly-snippets' },
     build = 'make install_jsregexp',
     opts = function()
         local types = require 'luasnip.util.types'
