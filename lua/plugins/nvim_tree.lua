5b42232e16ff16ee0acd6cf20501381676913c72
liubang <it.liubang@gmail.com>
2023-11-29 23:40:24 +0800

    perf(nvim-tree): disable sync_root_with_cwd

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -27,7 +27,7 @@ return {
     open_on_tab = false,
     respect_buf_cwd = false,
     sort_by = "name",
-    sync_root_with_cwd = true,
+    sync_root_with_cwd = false,
     view = {
       adaptive_size = false,
       centralize_selection = false,
@@ -60,10 +60,34 @@ return {
           nowait = true,
         }
       end
+
+      local git_add = function()
+        local node = api.tree.get_node_under_cursor()
+        local gs = node.git_status.file
+        -- If the current node is a directory get children status
+        if gs == nil then
+          gs = (node.git_status.dir.direct ~= nil and node.git_status.dir.direct[1])
+            or (node.git_status.dir.indirect ~= nil and node.git_status.dir.indirect[1])
+        end
+        -- If the file is untracked, unstaged or partially staged, we stage it
+        if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
+          vim.cmd("silent !git add " .. node.absolute_path)
+        -- If the file is staged, we unstage
+        elseif gs == "M " or gs == "A " then
+          vim.cmd("silent !git restore --staged " .. node.absolute_path)
+        end
+        api.tree.reload()
+      end
+
       -- default mappings
       api.config.mappings.default_on_attach(bufnr)
       vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
       vim.keymap.set("n", "s", api.node.open.horizontal, opts("Open: Horizontal Split"))
+      vim.keymap.set("n", "ga", git_add, opts("Git Add"))
+
+      api.events.subscribe(api.events.Event.FileCreated, function(file)
+        vim.cmd("edit " .. file.fname)
+      end)
     end,
     renderer = {
       add_trailing = false,
@@ -87,10 +111,10 @@ return {
         webdev_colors = true,
         git_placement = "after",
         show = {
+          git = true,
           file = true,
           folder = true,
           folder_arrow = true,
-          git = true,
         },
         padding = " ",
         symlink_arrow = " 󰁔 ",
