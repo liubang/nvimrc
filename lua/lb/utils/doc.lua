8ffe840c24d4e460b7d95138463823e3b063113c
liubang <it.liubang@gmail.com>
2024-05-17 15:16:11 +0800

    chore(license): Apache License 2.0

--- lua/lb/utils/doc.lua
+++ lua/lb/utils/doc.lua
@@ -1,11 +1,19 @@
---=====================================================================
+-- Copyright (c) 2024 The Authors. All rights reserved.
 --
--- doc.lua -
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
 --
--- Created by liubang on 2023/01/18 01:00
--- Last Modified: 2023/01/18 01:00
+--      https://www.apache.org/licenses/LICENSE-2.0
 --
---=====================================================================
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- Authors: liubang (it.liubang@gmail.com)
+
 local Docs = require("lazy.docs")
 local Util = require("lazy.util")
 local core = require("lazy.core.plugin").Spec.new({ import = "plugins" })
