b6327de06f621d30cee99ad16b80c2dc0156dd3a
liubang <it.liubang@gmail.com>
2024-05-17 15:16:11 +0800

    chore(license): Apache License 2.0

--- lua/lb/utils/comment.lua
+++ lua/lb/utils/comment.lua
@@ -1,11 +1,18 @@
--- =====================================================================
+-- Copyright (c) 2024 The Authors. All rights reserved.
 --
--- comment.lua -
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
 --
--- Created by liubang on 2020/12/11
--- Last Modified: 2022/10/31 00:08
+--      https://www.apache.org/licenses/LICENSE-2.0
 --
--- =====================================================================
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- Authors: liubang (it.liubang@gmail.com)
 
 local M = {}
 
