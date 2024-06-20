de1bee6cf327813540c44c1da80fb1045d402b1a
liubang <it.liubang@gmail.com>
2024-06-20 23:54:24 +0800

    fix(gitsigns): remove yadm

--- lua/plugins/gitsigns-nvim.lua
+++ lua/plugins/gitsigns-nvim.lua
@@ -50,7 +50,6 @@ return {
       linematch = 60,
     }, -- }}}
     current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-    yadm = { enable = false },
   },
   -- stylua: ignore
   keys = {
