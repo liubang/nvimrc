9a621beaa4f9656575e4f82637662f06cbd343fc
liubang <it.liubang@gmail.com>
2025-03-08 01:43:25 +0800

    perf(blink): fix lsp snippet missing placeholder

--- lua/plugins/lsp/customs.lua
+++ lua/plugins/lsp/customs.lua
@@ -15,12 +15,8 @@
 -- Authors: liubang (it.liubang@gmail.com)
 
 local M = {}
+local capabilities = require("blink.cmp").get_lsp_capabilities()
 local format = require("plugins.lsp.format")
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 
 local custom_attach = function(client, bufnr)
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
